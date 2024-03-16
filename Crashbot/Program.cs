8decb06073dd6ca7ff4892057a5d8517f845bc8b
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-03-16 00:59:56 -0400

    e

--- Crashbot/Program.cs
+++ Crashbot/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var socket = new SteamSocket(new SteamKit2.SteamUser.LogOnDetails()
+            {
+                Username = "tgo_inc",
+                Password = CredentialManager.GetPassword(),
+            });
+
+            socket.Connect();
+
+            socket.ConnectToTarget(new SteamKit2.SteamID(76561198299556567));
         }
     }
 }
