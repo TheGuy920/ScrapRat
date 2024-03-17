8cbb12053606f41d66f405a2bd25b8aac9b4cc85
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-03-17 03:32:46 -0400

    Update Program.cs

--- Crashbot/Program.cs
+++ Crashbot/Program.cs
@@ -90,7 +90,7 @@ namespace Crashbot
             Console.ReadKey();
 
             // confirm crashed
-            const int desiredTimeoutValue = 800;
+            const int desiredTimeoutValue = 1500;
             // Create an array of connection parameters (config values)
             SteamNetworkingConfigValue_t[] connectionParams = new SteamNetworkingConfigValue_t[1];
 
