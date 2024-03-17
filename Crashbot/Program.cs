c6e73039a6c58f9b7cda4831717f43930ba293e3
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-03-17 04:20:38 -0400

    Update Program.cs

--- Crashbot/Program.cs
+++ Crashbot/Program.cs
@@ -15,7 +15,7 @@ namespace Crashbot
             {
                 m_eDataType = ESteamNetworkingConfigDataType.k_ESteamNetworkingConfig_Int32,
                 m_eValue = ESteamNetworkingConfigValue.k_ESteamNetworkingConfig_TimeoutConnected,
-                m_val = new SteamNetworkingConfigValue_t.OptionValue { m_int32 = 50 }
+                m_val = new SteamNetworkingConfigValue_t.OptionValue { m_int32 = 100 }
             }
         ];
 
