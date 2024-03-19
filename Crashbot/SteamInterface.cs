7e094cd96b6be56e147ed9c3ea9903f65856ebe2
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-03-19 02:30:59 -0400

    Update SteamInterface.cs

--- Crashbot/SteamInterface.cs
+++ Crashbot/SteamInterface.cs
@@ -161,6 +161,7 @@ namespace Crashbot
                 if (cx is null || ix is null)
                 {
                     mega_victim.IsCrashing = false;
+                    Console.WriteLine($"Exiting early, likely due to token cancelation {interuptSource.IsCancellationRequested}", Verbosity.Debug);
                     return;
                 }
 
