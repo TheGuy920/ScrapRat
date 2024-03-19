7e66ba24717c796d1958d532cfb4abfa0c084cec
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-03-19 03:59:35 -0400

    close connection

--- Crashbot/SteamThread.cs
+++ Crashbot/SteamThread.cs
@@ -68,6 +68,7 @@ namespace Crashbot
 
                     if (cancel?.CanBeCanceled == true && cancel?.IsCancellationRequested == true)
                     {
+                        SteamNetworkingSockets.CloseConnection(conn, 0, "Cancelled", false);
                         connected.Set();
                         return;
                     }
