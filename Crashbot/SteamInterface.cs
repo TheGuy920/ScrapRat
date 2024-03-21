89a85e7930c88c9016c16f5020856b050bc9828a
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-03-21 05:41:09 -0400

    Update SteamInterface.cs

--- Crashbot/SteamInterface.cs
+++ Crashbot/SteamInterface.cs
@@ -229,7 +229,7 @@ namespace Crashbot
                 {
                     for (int i = 0; i < FUN_TIME; i++)
                     {
-                        this.SteamThread.SendMessageToConnection(conn, [], 1, 1);
+                        this.SteamThread.SendMessageToConnection(conn, [], 0, 3);
                         this.SteamThread.Get(SteamNetworkingSockets.FlushMessagesOnConnection, conn);
                         Task.Delay(20).Wait();
                     }
