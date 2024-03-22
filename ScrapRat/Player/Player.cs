011026069439a660bbc3e7b631802f312236dd78
TheGuy920 <53882381+TheGuy920@users.noreply.github.com>
2024-03-21 23:49:18 -0400

    Update Player.cs

--- ScrapRat/Player/Player.cs
+++ ScrapRat/Player/Player.cs
@@ -79,7 +79,7 @@ namespace ScrapRat
         {
             if (this.hSteamNetConnection.HasValue)
             {
-                SteamNetworkingSockets.CloseConnection(this.hSteamNetConnection.Value, 2, cmsg, true);
+                SteamNetworkingSockets.CloseConnection(this.hSteamNetConnection.Value, 0, cmsg, true);
 
                 if (!this.hSteamNetConnection.HasValue)
                     return;
